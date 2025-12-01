2215e2317d3302698a14d8b4f345cfb98f9f54b6
Niki-dev12 <nikola.popara@airvolute.com>
2025-12-01 14:28:57 +0100

    docker AppImage name using commit

--- deploy/docker/run-docker-ubuntu.sh
+++ deploy/docker/run-docker-ubuntu.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Define variables for better maintainability
@@ -21,3 +19,22 @@ docker run \
   -v "${SOURCE_DIR}:/project/source" \
   -v "${BUILD_DIR}:/project/build" \
   "${IMAGE_NAME}"
+
+APPIMAGE_PATH=$(find "${BUILD_DIR}" -maxdepth 1 -name "*.AppImage" | head -n 1)
+
+if [ -z "$APPIMAGE_PATH" ]; then
+    echo "ERROR: No AppImage found in ${BUILD_DIR}"
+    exit 1
+fi
+
+#current git commit short hash
+GIT_HASH=$(git -C "${SOURCE_DIR}" rev-parse --short HEAD)
+
+DIR=$(dirname "$APPIMAGE_PATH")
+BASE=$(basename "$APPIMAGE_PATH" .AppImage)
+NEW_NAME="${BASE}-${GIT_HASH}.AppImage"
+
+mv "$APPIMAGE_PATH" "${DIR}/${NEW_NAME}"
+
+echo "✓ AppImage renamed to:"
+echo "   ${DIR}/${NEW_NAME}"
